dc9a819787ca7676e42a1e078e5e5cd1cd202a56
Christian Monson <christianmonson@Christians-MacBook-Pro.local>
2015-03-06 17:14:24 -0700

    adds methods to timer file to make it work

--- Teacher Resources/Timer.h
+++ Teacher Resources/Timer.h
@@ -1,18 +1,26 @@
 //
-//  Timer.h
-//  Teacher Resources
+//  POTimer.h
+//  The Pomodoro
 //
-//  Created by Ethan Hess on 3/6/15.
-//  Copyright (c) 2015 PJayRushton. All rights reserved.
+//  Created by sombra on 2015-02-16.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
+static NSString * const secondTickNotification;
+static NSString * const currentRoundNotification;
+static NSString * const roundCompleteNotification;
 
 
-@interface Timer : NSObject
+@interface POTimer : NSObject
 
-@property (nonatomic, assign) NSInteger *minutes;
-@property (nonatomic, assign) NSInteger *seconds;
+@property (nonatomic,assign) NSInteger minutes;
+@property (nonatomic,assign) NSInteger seconds;
+
++ (POTimer *)sharedInstance;
+
+- (void)startTimer;
+- (void)cancelTimer;
 
 @end
--- Teacher Resources/Timer.m
+++ Teacher Resources/Timer.m
@@ -1,13 +1,78 @@
 //
-//  Timer.m
-//  Teacher Resources
+//  POTimer.m
+//  The Pomodoro
 //
-//  Created by Ethan Hess on 3/6/15.
-//  Copyright (c) 2015 PJayRushton. All rights reserved.
+//  Created by sombra on 2015-02-16.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
 //
 
 #import "Timer.h"
 
-@implementation Timer
+static NSString * const secondTickNotification = @"secondTick";
+static NSString * const currentRoundNotification = @"currentRound";
+static NSString * const roundCompleteNotification = @"roundComplete";
+
+@interface POTimer ()
+
+@property (nonatomic,assign) BOOL isOn;
+
+@end
+
+
+
+@implementation POTimer
+
++ (POTimer *)sharedInstance {
+    static POTimer *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[POTimer alloc] init];
+    });
+    return sharedInstance;
+}
+
+- (void)startTimer {
+    self.isOn = YES;
+    [self isActive];
+}
+
+- (void)cancelTimer {
+    self.isOn = NO;
+    [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(decreaseSecond) object:nil];
+}
+
+- (void)endTimer {
+    self.isOn = NO;
+    [[NSNotificationCenter defaultCenter] postNotificationName:roundCompleteNotification object:nil];
+    
+}
+
+- (void)decreaseSecond {
+    
+    if (self.seconds > -1){
+        self.seconds --;
+    }
+    
+    if (self.minutes > 0 || (self.minutes == 0 && self.seconds > -1)) {
+        if (self.seconds == -1) {
+            self.seconds = 59;
+            self.minutes--;
+        }
+        
+        [[NSNotificationCenter defaultCenter]postNotificationName:secondTickNotification object:nil];
+    } else {
+        if (self.seconds == -1){
+            [self endTimer];
+        }
+    }
+    
+}
+
+- (void)isActive {
+    if (self.isOn) {
+        [self decreaseSecond];
+        [self performSelector:@selector(isActive) withObject:nil afterDelay:1.0];
+    }
+}
 
 @end
