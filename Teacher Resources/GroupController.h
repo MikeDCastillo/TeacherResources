f177e8ca7eabe6890cbbb19fdd57b0de2ab2cc13
prush08 <parkerrushton@gmail.com>
2015-03-19 10:37:19 -0600

    Changes Temporary Student List back from Mutable back to NSArray

--- Teacher Resources/GroupController.h
+++ Teacher Resources/GroupController.h
@@ -19,7 +19,7 @@ static NSString * const studentKey = @"studentKey";
 @property (nonatomic, strong, readonly) NSArray *groups;
 @property (nonatomic, strong) Group *group;
 @property (nonatomic, strong) Member *member;
-@property (nonatomic, strong) NSMutableArray *temporaryStudentList;
+@property (nonatomic, strong) NSArray *temporaryStudentList;
 
 + (GroupController *)sharedInstance;
 
